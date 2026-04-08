0757483a1abc2233995a8750c21b3a538defe76d
SergioGlez <serchgonzalez1997@gmail.com>
2026-04-07 23:54:46 -0600

    Fix

--- backend/app/api/routes/generate.py
+++ backend/app/api/routes/generate.py
@@ -5,7 +5,7 @@ from app.models.generate import GenerateTextRequest
 router = APIRouter(prefix="/collections", tags=["generate"])
 
 
-@router.post("/{collection_id}/text")
+@router.post("/{collection_id}/generate/text")
 async def generate(request: GenerateTextRequest, collection_id: str):
     return {
         "message": await generate_response(request.query, collection_id=collection_id),
