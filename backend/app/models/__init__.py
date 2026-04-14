7af67aea946a2613dd6805df9f0a701b27e803e7
SergioGlez <serchgonzalez1997@gmail.com>
2026-04-14 12:06:16 -0600

    Update models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,19 +1,3 @@
-from app.models.collections import (
-    Collection,
-    CreateCollectionRequest,
-    CollectionResponse,
-    CollectionListResponse,
-)
-from app.models.documents import (
-    Document,
-    DocumentResponse,
-    DocumentListResponse,
-)
-from app.models.entities import (
-    Entity,
-    CreateEntityRequest,
-    UpdateEntityRequest,
-    EntityResponse,
-    EntityListResponse,
-)
-from app.models.generate import GenerateTextRequest, GenerateTextResponse
+from app.models.collections import Collection
+from app.models.documents import Document
+from app.models.entities import Entity
