47097ceeedfdd3d6409527b2697a61af8877d290
SergioGlez <serchgonzalez1997@gmail.com>
2026-04-14 19:23:05 -0600

    Enitity Text Draft

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.collections import Collection
 from app.models.documents import Document
 from app.models.entities import Entity
+from app.models.entity_text_draft import EntityTextDraft
