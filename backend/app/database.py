38d5f3d53f98aca1d24f0695ec2dc63fb205a7c4
SergioGlez <serchgonzalez1997@gmail.com>
2026-04-15 13:02:33 -0600

    Changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ from config import settings
 connect_args = {}
 if settings.database_url.startswith("sqlite"):
     connect_args["check_same_thread"] = False
- 
+
 engine = create_engine(
     settings.database_url,
     connect_args=connect_args,
