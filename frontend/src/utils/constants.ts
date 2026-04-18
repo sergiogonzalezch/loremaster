c673a3893b934907f77c7da1ba4da4a2daa41817
Mike Nieva <m@mikenieva.com>
2026-04-18 10:43:07 -0600

    Revision 18 de abril

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -6,3 +6,7 @@ export const ENTITY_TYPE_BADGE: Record<EntityType, string> = {
   faction: "warning",
   item: "info",
 };
+
+export const MAX_PENDING_DRAFTS = 5;
+export const MIN_QUERY_LENGTH = 5;
+export const MAX_GENERATION_TOKENS = 2000;
